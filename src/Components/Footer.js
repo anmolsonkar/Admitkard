87fef10c5b02319c753aed4434e60cef80676709
Anmol Sonkar <anmolsonkar742@gmail.com>
2022-07-31 11:21:26 +0530

    Initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="flex bg-[#2D438B] mx-auto text-white">
+        Footer
+    </div>
+  )
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className="flex bg-[#2D438B] mx-auto text-white">
+        Main
+    </div>
+  )
+}
