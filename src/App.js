55171461592995034e652a1a1e45d73b42de7ab6
Anmol Sonkar <anmolsonkar742@gmail.com>
2023-02-13 18:37:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Manage from "./Components/Manage";
-import Tools from "./Components/Tools";
 import About from "./Components/About";
 import Home from "./Components/Home";
 import Calculator from "./Components/Calculator";
@@ -20,7 +19,6 @@ function App() {
           <Route path="/Home" element={<Home />} />
           <Route path="/Manage" element={<Manage />} />
           <Route path="/About" element={<About />} />
-          <Route path="/Tools" element={<Tools />} />
           <Route path="/Calculator" element={<Calculator />} />
         </Routes>
       </Router>
